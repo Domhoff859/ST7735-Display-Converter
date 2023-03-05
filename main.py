5d09cf464f4a7084a8da2d4945cf2d323039b32b
Domhoff859 <88834154+Domhoff859@users.noreply.github.com>
2023-03-06 00:22:35 +0100

    Added Comments

--- main.py
+++ main.py
@@ -3,38 +3,18 @@ from pathlib import Path
 import numpy as np
 
 if __name__ == '__main__':
-
+    # Read Image
     current_path = Path(__file__).parent
     img1 = cv2.imread(str(current_path.joinpath('vac2.bmp')), cv2.IMREAD_COLOR)
-    img2 = cv2.imread(str(current_path.joinpath('nojuice.png')), cv2.IMREAD_COLOR)
 
-
+    # Convert to RGB565
     R5 = (img1[...,0]>>3).astype(np.uint16) << 11
     G6 = (img1[...,1]>>2).astype(np.uint16) << 5
     B5 = (img1[...,2]>>3).astype(np.uint16)
     RGB565 = R5 | G6 | B5
-    print(RGB565.shape)
-
-    #np.savetxt("test.txt", RGB565.astype((np.uint16)), delimiter=',', fmt='%u')
-
+    
+    # Add '{' to begin and '},' to end of each line
     with open('vac.txt', "w") as file:
-        for row in RGB565:
-            file.write("{")
-            for i, value in enumerate(row):
-                if i < len(row) - 1:
-                    file.write(f"{value}, ")
-                else:
-                    file.write(f"{value}")
-            file.write("},\n")
-
-
-    R5 = (img2[...,0]>>3).astype(np.uint16) << 11
-    G6 = (img2[...,1]>>2).astype(np.uint16) << 5
-    B5 = (img2[...,2]>>3).astype(np.uint16)
-    RGB565 = R5 | G6 | B5
-    print(RGB565.shape)
-
-    with open('juice.txt', "w") as file:
         for row in RGB565:
             file.write("{")
             for i, value in enumerate(row):
